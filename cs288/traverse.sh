8c661b25bebf0952c4a30490237a630ad34e89c5
Alexander Naspo <alex.naspo@gmail.com>
2013-02-05 16:36:04 -0500

    removed count function, removed echo

--- cs288/traverse.sh
+++ cs288/traverse.sh
@@ -2,26 +2,17 @@
 function traverse(){   
     local dir=$1
     open=`ls $dir/`
-
+    
     for x in ${open[@]}; do
-        if [ -d "$dir/$x" ]; then
-            echo $dir/$x
-            traverse $dir/$x
-        else
-            echo $dir/$x
+        echo $dir/$x
+        if [ -d "$dir/$x" ]; then            
+            traverse $dir/$x        
         fi        
-        count
     done
 }
 
 function main(){
-    count=0
     traverse $1
 }
 
-function count(){
-    count=`expr $count + 1`
-}
-
 main $1
-echo $count
