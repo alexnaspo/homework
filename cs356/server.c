ab03ef506d38a20d05a982249943dea7cdaeb207
Alexander Naspo <alex.naspo@gmail.com>
2013-03-03 15:44:00 -0500

    removed imports

--- cs356/server.c
+++ cs356/server.c
@@ -1,28 +1,28 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <unistd.h>
-#include <sys/types.h> 
+// #include <unistd.h>
+// #include <sys/types.h> 
 #include <sys/socket.h>
 #include <netinet/in.h>
 
 int main(int argc, char *argv[])
 {
   struct sockaddr_in server_address, client_address;
-  int socket_one, new_socket, port_num;
+  int socket_one, new_socket, port_num, temp;
   socklen_t clilen;
   char msg_buffer[256];
-  int temp;
 
   if (argc < 2)
   {
-    printf("%s\n", "Please provide the port number as the second arg");
+    printf("%s\n", "Please provide the port number");
   }
   socket_one = socket(AF_INET, SOCK_STREAM, 0);
   if( socket_one < 0) {
-    printf("%s\n", "Error creating the Socket");
+    printf("%s\n", "Error creating the socket");
   }
 
+  //fill struct sockaddr_in with fields
   bzero((char *) &server_address, sizeof(server_address));
   port_num = atoi(argv[1]);
   server_address.sin_family = AF_INET;
@@ -30,28 +30,23 @@ int main(int argc, char *argv[])
   server_address.sin_port = htons(port_num);
 
   if (bind(socket_one, (struct sockaddr *) &server_address, sizeof(server_address)) < 0){
-    printf("%s\n", "ERROR on binding"); 
+    printf("%s\n", "Connection error"); 
   }
-
+  
   listen(socket_one, 5);
+  printf("%s\n", "Waiting for Message...");
   clilen = sizeof(client_address);
   new_socket = accept(socket_one, (struct sockaddr *) &client_address, &clilen);
 
-  if (new_socket < 0) {
-    printf("%s\n", "ERROR on accept");
-  }
-
+  //set msg_buffer values to 0, then save message
   bzero(msg_buffer, 256);
   temp = read(new_socket, msg_buffer, 255);
-  // if (temp < 0) {
-  //   printf("%s\n", "ERROR reading from socket");
-  // }
 
   printf("Response message: %s\n",msg_buffer);
   temp = write(new_socket, "I got your message",18);
 
   if (temp < 0) {
-    printf("%s\n", "ERROR writing to socket");
+    printf("%s\n", "Error occured writing to socket");
   }
 
   close(new_socket);
