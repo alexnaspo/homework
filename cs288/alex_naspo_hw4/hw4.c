c7d4debb87f9d079d4650f2abb27a47f848a8b18
Alexander Naspo <alex.naspo@gmail.com>
2013-03-14 23:23:07 -0400

    radix sort now working

--- cs288/alex_naspo_hw4/hw4.c
+++ cs288/alex_naspo_hw4/hw4.c
@@ -7,6 +7,7 @@
 void build_a_lst(),print_a_line(),print_lst();
 void insert_at_end();
 void int_radix_sort();
+void radix_wrapper();
 
 struct clip {
   int views;
@@ -18,9 +19,10 @@ struct clip {
 
 int main(int argc, char **argv) {
   build_a_lst(*(argv+1));
-  //print_lst(head);    /* prints the table */
+  print_lst(head);    /* prints the table */
   int_radix_sort(head);
-  //print_lst(head);    /* prints the table but now sorted */
+  printf("%s\n", "=========");
+  print_lst(head);    /* prints the table but now sorted */
   return 0;
 }
 
@@ -81,6 +83,7 @@ void insert_at_end(char *s) {
       cp = cp->next;
     }
     cp->next = tp;
+
   }
 
 
@@ -91,102 +94,83 @@ void int_radix_sort(struct clip *cp) {
     you will need a wrapper here for radix_sort only, without having to involve
     struct clip or linked list
   */
-  int rnd = 0;
-  int cnt[9999];
-  int map[9999];
-  int buf[9999];
-  int lst[9999];
+  struct clip *dest[9999];
+  struct clip *source[9999];
   int i = 0;
-  int j;
-  int c;
-  int bucket = 16;
-  int mask = bucket - 1;
   int flag = 0;
+  int rnd = 0;
 
   while(cp->next != NULL){
-    lst[i] = cp->views;
+    source[i] = cp;
     cp = cp->next;
     i = i + 1;
   }
 
   int n = i;
-  // need to rotate desination/source each time
-  // aka rotate lst/buffer
-  // should have a flag
-   
-  
-  while(rnd < n){ 
-    for (i = 0; i < bucket; ++i)
-    { 
-      map[i] = 0;
-      cnt[i] = 0;
-    } 
-    
-    for (i = 0; i < n; ++i)
-    {
-      if(flag == 0) { 
-        // printf("%x\n", lst[i]);
-        // printf("%x\n", (lst[i] >> 4 * rnd & mask));
-        // printf("%s\n", "");        
-        cnt[(lst[i] >> 4 * rnd) & mask]++;
-      }
-      else {
-        // printf("%x\n", buf[i]);
-        // printf("%x\n", (buf[i] >> 4 * rnd & mask));
-        // printf("%s\n", "");        
-        cnt[(buf[i] >> 4 * rnd) & mask]++;
-      }
-    }
 
-    for (i = 0; i < bucket; ++i)
-    {
-      // printf("%i\n", cnt[i]);
-    }
-    
-    for (i = 0; i < bucket; i++) {   
-      map[i] = map[i-1] + cnt[i-1];
-    }
-
-    for (j = 0; j < n; j++)
-    {
-      if(flag == 0) { 
-        buf[map[(lst[j] >> (4 * rnd)) & mask]++] = lst[j];   
-      } else {
-        lst[map[(buf[j] >> (4 * rnd)) & mask]++] = buf[j];   
-      }
-    }
-    printf("%s\n", "=======");      
-
-    if (flag == 0)
-    {
-      flag = 1; 
-      printf("%s\n", "buf");
-      for (c = 0; c < n; c++){
-        
-        printf("%i\n", buf[c]);
-      }      
+  while (rnd < n){
+    if (flag == 0) {
+      radix_wrapper(source, dest, n, rnd);
+      flag = 1;  
     } else {
+      radix_wrapper(dest, source, n, rnd);
       flag = 0;
-      printf("%s\n", "list");
-      for (c = 0; c < n; c++){
-        printf("%i\n", lst[c]);
-      }
-    }
+    }  
     rnd = rnd + 1;
-    printf("%s\n", "=======");  
   }
-  
+
+  // re-link clips
+  head = source[0];
+  cp = head;
+  for ( i = 1; i <= n; i++)
+  {
+    cp->next = source[i];
+    if (i == n)
+      cp->next = NULL;
+    else
+      cp = cp->next;  
+  }
+}
+
+void radix_wrapper(struct clip *source[], struct clip *dest[], int n, int rnd){
+  int i;
+  int cnt[9999];
+  int map[9999];
+  int bucket = 256;
+  int mask = bucket - 1;
+
+  for (i = 0; i < bucket; ++i)
+  { 
+    map[i] = 0;
+    cnt[i] = 0;
+  }
+
+  for (i = 0; i < n; i++)
+  {
+    cnt[(source[i]->views >> 8 * rnd) & mask]++;
+  }
+
+  for (i = 1; i < bucket; i++) {   
+    map[i] = map[i-1] + cnt[i-1];
+  }  
+
+  for (i = 0; i < n; i++) {
+    dest[map[(source[i]->views >> (8 * rnd)) & mask]++] = source[i];  
+  }
 
 }
 
 /* prints all the users */
 void print_lst(struct clip *cp) {
+  printf("%i\n", cp->views);
   while(cp->next != NULL){
     cp = cp->next;
+    printf("%i\n", cp->views);
+    
     // printf("%s\n", cp->user);
     // printf("%s\n", cp->duration);
     // printf("%s\n", cp->title);
-    printf("%i\n", cp->views);
+    
   }
 }
 
